e4ee93762da63706ad0e758b60f58b2ac66d020c
MacbClarke <macbclarke@gmail.com>
2022-05-09 15:22:28 +0800

    fix: fixed frame error

--- app.ts
+++ app.ts
@@ -7,27 +7,15 @@ const app = express();
 const cors = require('cors');
 const server = require('http').Server(app);
 const io = require("socket.io")(server, {cors: true});
-// const nsp = io.of('/chat');
-const { ExpressPeerServer } = require('peer');
+const { PeerServer } = require('peer');
 
 app.use(cors());
-// app.all('*', function (req, res, next) {
-//     res.header('Access-Control-Allow-Origin', '*')
-//     res.header("Access-Control-Allow-Headers", " Origin, X-Requested-With, Content-Type, Accept");
-//     res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS')
-//     res.header('X-Powered-By', ' 3.2.1')
-//     res.header('Content-Type', 'application/json;charset=utf-8')
-//     next()
-// })
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-
-app.use('/peer', ExpressPeerServer(server, {
-    path: '/'
-}));
 
 app.use('/', route);
 
 io.on('connection', socketController)
 
 server.listen(8848)
+PeerServer({port: 8849, path: '/peer'});
